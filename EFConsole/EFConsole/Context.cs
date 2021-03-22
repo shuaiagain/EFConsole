6ce1cec0ad524cc1a6137b5d8ef030b4e2f9e9d8
ssding <dingss@gjmetal.com>
2021-03-22 17:34:47 +0800

    feat(ef)：ef控制台demo

---
+++ EFConsole/EFConsole/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsole
+{
+    public class Context : DbContext
+    {
+        public Context() : base("name=EFConsole")
+        {
+        }
+
+        public DbSet<Donator> Donators { get; set; }
+    }
+}
---
+++ EFConsole/EFConsole/Donator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsole
+{
+    public class Donator
+    {
+        public int DonatorId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime DonateTime { get; set; }
+    }
+}
